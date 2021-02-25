7e1ec224c5891da74ca24a15a48141db9017f009
Carlos Berti <carlosberti182@gmail.com>
2021-02-25 03:03:52 -0300

    Atividade 12 (#7)
    * chore: add jest mongodb
    
    * chore: configure jest mongodb
    
    * chore: add mongodb driver
    
    * feat: mongodb helper module
    
    * chore: remove globalConfig from version
    
    * feat: add user and verify if it exists on mongodb
    
    * feat: find all users in a mongodb repository
    
    * test: find all users in a mongodb repository
    
    * refactor: use mongodb user repository on register user controller factory
    
    * feat: connect to mongodb before litening

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ const { pathsToModuleNameMapper } = require('ts-jest/utils')
 const { compilerOptions } = require('./tsconfig')
 
 module.exports = {
+  preset: '@shelf/jest-mongodb',
   // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>/test'],
   // The directory where Jest should output its coverage files
@@ -16,8 +17,6 @@ module.exports = {
     '!**/test/**',
     '!**/confis/**'
   ],
-  // The test environment that will be used for testing
-  testEnvironment: 'node',
   // A map from regular expressions to paths to transformers
   transform: {
     '.+\\.ts$': 'ts-jest'
---
+++ test/external/repositories/mongodb/mongodb-user-repository.spec.ts
@@ -0,0 +1,44 @@
+import { MongoHelper } from '@/external/repositories/mongodb/helper'
+import { MongodbUserRepository } from '@/external/repositories/mongodb'
+
+describe('Mongodb User repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    MongoHelper.clearCollection('users')
+  })
+
+  it('when user is added, it should exist', async () => {
+    const userRepository = new MongodbUserRepository()
+    const user = {
+      name: 'any_name',
+      email: 'any@mail.com'
+    }
+    await userRepository.add(user)
+
+    expect(await userRepository.exists(user)).toBeTruthy()
+  })
+
+  it('find all users should return all added users', async () => {
+    const userRepository = new MongodbUserRepository()
+    await userRepository.add({
+      name: 'any_name',
+      email: 'any@mail.com'
+    })
+    await userRepository.add({
+      name: 'second_name',
+      email: 'second@mainModule.com'
+    })
+
+    const users = await userRepository.findAllUsers()
+
+    expect(users[0].name).toEqual('any_name')
+    expect(users[1].name).toEqual('second_name')
+  })
+})
